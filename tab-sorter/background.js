5068a1df30031d2c6e41d034b9fb95b43a02e4a4
yingchengpeng <yingchengpeng@youzan.com>
2024-08-21 14:42:14 +0800

    修复排序逻辑

--- tab-sorter/background.js
+++ tab-sorter/background.js
@@ -1,28 +1,38 @@
 chrome.action.onClicked.addListener(async (tab) => {
     chrome.tabs.query({ currentWindow: true }, function(tabs) {
-        sortAndMoveTabs(tabs).then(sortedTabs => {
-            console.log(sortedTabs);
-        });
-    });
-});
+        sortAndMoveTabs(tabs)
+    })
+})
 
 
 const sortAndMoveTabs = function(tabs) {
     const sortedTabs = tabs.slice().sort((a, b) => {
-        if (a.title < b.title) {
-            return -1;
-        }
-        if (a.title > b.title) {
-            return 1;
-        }
-        return 0;
-    });
+        let preTitle = a.title
+        preTitle = preTitle.toLowerCase()
+        let nextTitle = b.title
+        nextTitle = nextTitle.toLowerCase()
+        return sortStrings(preTitle, nextTitle)
+    })
 
-    const tabPromises = [];
+    const tabPromises = []
 
     for (let i = 0; i < sortedTabs.length; i++) {
-        tabPromises.push(chrome.tabs.move(sortedTabs[i].id, { index: i }));
+        tabPromises.push(chrome.tabs.move(sortedTabs[i].id, { index: i }))
     }
 
-    return Promise.all(tabPromises).then(() => sortedTabs);
+    return Promise.all(tabPromises).then(() => sortedTabs)
+}
+
+
+const sortStrings = function(preStr, nextStr) {
+    // 比较两个字符串的第一个字符
+    if (preStr[0] < nextStr[0]) {
+        return -1;
+    }
+    if (preStr[0] > nextStr[0]) {
+        return 1;
+    }
+
+    // 如果第一个字符相同，继续比较下一个字符
+    return sortStrings(preStr.slice(1), nextStr.slice(1));
 }
